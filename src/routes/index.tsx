d1bec29b088f2bbd1dfe3eda92207b61e924398e
luffy <60225359+luffyxxsenpai@users.noreply.github.com>
2023-11-09 12:29:22 +0530

    added gdrive

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,8 @@ export default function Home() {
       <Title>Hello World</Title>
       <h1>Hello Space!</h1>
       <p><a href="https://ai.luffysenpai.space" target="_blank">ONE-IMAGE</a></p>
-      <p><a href="https://notes.luffysenpai.space" target="_blank">NOTES</a></p>    
+      <p><a href="https://notes.luffysenpai.space" target="_blank">NOTES</a></p>
+      <p><a href="https://drive.google.com/drive/folders/1MA3LTJ1yhpo0P0yRCWHktQSlFBKvFeH2" target="_blank">BCA</a></p> 
       <Counter />
     </main>
   );
