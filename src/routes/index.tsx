d41f8cfe7c5afd7d5095b1c70819b16a07b6f43b
luffy <60225359+luffyxxsenpai@users.noreply.github.com>
2023-11-09 12:32:22 +0530

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,7 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
+      <Title>luffy senpai</Title>
       <h1>Hello Space!</h1>
       <p><a href="https://ai.luffysenpai.space" target="_blank">ONE-IMAGE</a></p>
       <p><a href="https://notes.luffysenpai.space" target="_blank">NOTES</a></p>
