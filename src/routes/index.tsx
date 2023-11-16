809decd589b9622768fb89429591d35bbe031299
luffy <60225359+luffyxxsenpai@users.noreply.github.com>
2023-11-16 08:45:09 +0530

    Whatsapp link

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,8 @@ export default function Home() {
       <h1>Hello Space!</h1>
       <p><a href="https://ai.luffysenpai.space" target="_blank">ONE-IMAGE</a></p>
       <p><a href="" target="_blank">USELESS-BUTTON</a></p>
-      <p><a href="https://drive.google.com/drive/folders/1MA3LTJ1yhpo0P0yRCWHktQSlFBKvFeH2" target="_blank">BCA</a></p> 
+      <p><a href="https://drive.google.com/drive/folders/1MA3LTJ1yhpo0P0yRCWHktQSlFBKvFeH2" target="_blank">BCA</a></p>
+      <p><a href="https://chat.whatsapp.com/Bv9m21dyKfd55y5oPK4Jwb" target="_blank">BCA-GROUP</a></p>
       <Counter />
     </main>
   );
